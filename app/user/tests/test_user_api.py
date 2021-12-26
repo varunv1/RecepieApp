9bbff0549120e7c5715d85c835a85f5475167454
Varun <varunv1a2r3@gmail.com>
2021-12-26 22:22:27 +0530

    Creating recipe app without docker

--- app/user/tests/test_user_api.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-from rest_framework.test import  APIClient
-from rest_framework import status
